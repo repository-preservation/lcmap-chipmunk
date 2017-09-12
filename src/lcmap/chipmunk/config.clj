19726cb6c46ad2c5809373699fe4b3567a1c0f7b
Jonathan Morton <jonathan.morton.ctr@usgs.gov>
2017-09-12 14:11:05 -0500

    Support comma-separated list of Cassandra hosts

--- src/lcmap/chipmunk/config.clj
+++ src/lcmap/chipmunk/config.clj
@@ -1,5 +1,17 @@
 (ns lcmap.chipmunk.config
-  ""
+  "Configuration related functions.
+
+  Values are obtained from ENV variables and/or profiles.clj. These are:
+
+  | ENV            | Description                 |
+  | -------------- | --------------------------- |
+  | `HTTP_PORT`    | Chipmunk's HTTP listener    |
+  | `DB_HOST`      | Cassandra node (just one)   |
+  | `DB_USER`      | Cassandra username          |
+  | `DB_PASS`      | Cassandra password          |
+  | `DB_PORT`      | Cassandra cluster port      |
+  | `DB_KEYSPACE`  | Chipmunk's keyspace name    |
+  "
   (:require [environ.core :as environ]
             [mount.core :as mount]))
 
@@ -7,10 +19,18 @@
 (def config (select-keys environ/env [:db-host :db-user :db-pass :db-port :db-keyspace :http-port]))
 
 
+(defn string->vector
+  "Split string on comma into vector"
+  [s]
+  (->> (clojure.string/split s #"[, ]+")
+       (map clojure.string/trim)
+       (vec)))
+
+
 (defn alia-config
   "Produce alia specific configuration from environment."
   []
-  {:contact-points (-> :db-host config vector)
+  {:contact-points (-> :db-host config string->vector)
    :credentials    {:user (:db-user config)
                     :password (:db-pass config)}
    :query-options  {:consistency :quorum}})
