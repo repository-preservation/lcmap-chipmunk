b7c43fca5f162401e1409460bba890b5729fe3f9
Jonathan Morton <jonathan.morton.ctr@usgs.gov>
2017-11-28 13:46:27 -0600

    WIP: Remove scratch from dev/user.clj

--- dev/user.clj
+++ dev/user.clj
@@ -48,38 +48,3 @@
     (map registry/add! layers))
   (let [layers (-> "registry.aux.edn" clojure.java.io/resource slurp edn/read-string)]
     (map registry/add! layers)))
-
-
-(comment
-  "Ingest some test data."
-  (let [base "http://guest:guest@localhost:9080"
-        tile "LC08_CU_027009_20130701_20170729_C01_V01"
-        path "%s/%s_SR.tar/%s_%s.tif"
-        f1 (format path base tile tile "SRB1")
-        f2 (format path base tile tile "PIXELQA")
-        f3 (format path base tile tile "LINEAGEQA")]
-    [(core/ingest f1)
-     (core/ingest f2)
-     (core/ingest f3)]))
-
-
-(comment
-  (let [url "http://localhost:9080/LC08_CU_027009_20130701_20170729_C01_V01_SR.tar"]
-    (ard/ard-metadata url)))
-
-
-(comment
-  (let [url "http://localhost:9080/LC08_CU_027009_20130701_20170729_C01_V01.xml"
-        doc (-> url ard/ard-metadata-get xpath/xml->doc)]
-    {:product_id (first (xpath/$x:text+ "//product_id" doc))
-     :satellite  (first (xpath/$x:text+ "//satellite" doc))
-     :instrument (first (xpath/$x:text+ "//instrument" doc))
-     :collection (first (xpath/$x:text+ "//level1_collection" doc))
-     :version    (first (xpath/$x:text+ "//ard_version" doc))
-     :region     (first (xpath/$x:text+ "//region" doc))
-     :tile_h     (first (xpath/$x:attrs* "//tile_grid" doc :h))
-     :tile_v     (first (xpath/$x:attrs* "//tile_grid" doc :v))
-     ;; Scene metadata
-     :scene_center_time (first (xpath/$x:text+ "//scene_center_time" doc))
-     :acquisition_date  (first (xpath/$x:text+ "//acquisition_date" doc))
-     }))
