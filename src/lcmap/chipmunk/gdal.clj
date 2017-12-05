a4df801c3afddc661bf63e236619bdafee745511
Jonathan Morton <jonathan.morton.ctr@usgs.gov>
2017-12-05 11:13:33 -0600

    Explain GDAL ns

--- src/lcmap/chipmunk/gdal.clj
+++ src/lcmap/chipmunk/gdal.clj
@@ -7,19 +7,26 @@
            [org.gdal.gdalconst gdalconst]))
 
 
+;; ## Init
+;;
+;; This makes it easier to use Java GDAL libraries without
+;; having to set environment variables. These are typical
+;; install locations of GDAL libs on CentOS and Ubuntu.
+;;
+;; Before GDAL can open files, drivers must be registered.
+;; Selective registration is more tedious and error prone,
+;; so we just register all drivers.
+;;
+;; If anything goes wrong, a helpful string is printed to
+;; stdout (not a log file).
+;;
+
 (defn init
   "Initialize GDAL drivers."
   []
   (try
-    ;; This makes it easier to use Java GDAL libraries without
-    ;; having to set environment variables. These are typical
-    ;; install locations of GDAL libs on CentOS and Ubuntu.
     (util/amend-usr-path ["/usr/lib/java/gdal" "/usr/lib/jni"])
-    ;; Before GDAL can open files, drivers must be registered.
-    ;; Selective registration is more tedious and error prone,
-    ;; so we just register all drivers.
     (gdal/AllRegister)
-    ;; If something does go wrong a helpful error is displayed.
     (catch RuntimeException e
       (binding [*out* *err*]
         (println (str "Could not update paths to native libraries. "
@@ -29,9 +36,27 @@
       (import org.gdal.gdal.gdal))))
 
 
+;; ## State
+;;
+;; A mount state is defined so that GDAL is initialized like
+;; everything else (DB connections, HTTP listeners, etc...)
+;;
+
 (mount/defstate gdal-init
   :start (init))
 
+
+;; ## VSI Prefixes
+;;
+;; This function adds transparent support for URLs that appear
+;; to need `vsitar` and `vsicurl` prefixes.
+;;
+;; GDAL provides a variety of ways to access data that is not
+;; on a local file-system. When Chipmunk ingests data, it is
+;; preferable to avoid downloading, unarchiving, uncompressing,
+;; and removing temporary files.
+;;
+;;
 
 (defn add-vsi-prefix
   "Make an ordinary URL a GDAL path with VSI prefixes."
@@ -56,15 +81,13 @@
   (if ds (.delete ds)))
 
 
-(defmacro with-data
-  "Use an already open dataset and then close it."
-  [bindings & body]
-  `(let ~(subvec bindings 0 2)
-     (try
-       (do ~@body)
-       (finally
-         (close ~(bindings 0))))))
-
+;; ## Java Interop. Functions
+;;
+;; These functions provide a layer on-top of the native Java
+;; methods provided by the GDAL library. A variety of functions
+;; work directly with arrays; these attempt to make things
+;; easier to work with.
+;;
 
 (defn geo-transform
   "Transform raster pixel and line to coordinate system (x,y)"
@@ -75,35 +98,49 @@
     [(first gx) (first gy)]))
 
 
-(defn get-projection [ds]
+(defn get-projection
+  "Get a dataset's projection as well known text."
+  [ds]
   (.GetProjection ds))
 
 
-(defn get-raster-count [ds]
+(defn get-raster-count
+  "Get the number of raster contained in the dataset."
+  [ds]
   (.GetRasterCount ds))
 
 
-(defn get-raster-x-size [ds]
+(defn get-raster-x-size
+  "Get the dataset's number of pixels in the raster's x dimension."
+  [ds]
   (.GetRasterXSize ds))
 
 
-(defn get-raster-y-size [ds]
+(defn get-raster-y-size
+  "Get the dataset's number of pixels in the raster's y dimension."
+  [ds]
   (.GetRasterYSize ds))
 
 
-(defn get-geo-transform [ds]
+(defn get-geo-transform
+  "Get the dataset's affine transform matrix as an array."
+  [ds]
   (.GetGeoTransform ds))
 
 
-(defn get-geo-transform-vec [ds]
+(defn get-geo-transform-vec
+  "Get the dataset's affine transform matrix as a vector."
+  [ds]
   (vec (get-geo-transform ds)))
 
 
 (defn get-metadata [ds]
+  "Get the dataset's metadata as a map."
   (.GetMetadata_Dict ds))
 
 
 (defn band
+  "Get the dataset's band indicated by ix, a one-based index."
   ([ds ix]
    (.GetRasterBand ds ix))
   ([ds]
@@ -123,6 +160,12 @@
      (.ReadRaster_Direct band xoff yoff xsize ysize buf-type))))
 
 
+;; ## Convenience Functions
+;;
+;; These function build on the basic GDAL library functions,
+;; you won't find anything equivalent in the Java library.
+;;
+
 (defn raster-seq
   "Provide a seq of raster data with additional context."
   [band {:keys [xstart ystart xstop ystop xstep ystep]}]
@@ -132,8 +175,19 @@
       {:raster-x x :raster-y y :data (reader x y)})))
 
 
+(defmacro with-data
+  "Use an already open dataset and then close it."
+  [bindings & body]
+  `(let ~(subvec bindings 0 2)
+     (try
+       (do ~@body)
+       (finally
+         (close ~(bindings 0))))))
+
+
 (defn dataset-info
-  ""
+  "Get some information about the dataset at path, more convenient
+   than building things by hand."
   [path]
   (with-data [data (open path)]
     (if data
