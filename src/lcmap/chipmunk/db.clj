33fb94d717dc1ccc1ef40f140b11819bfa905c13
Jonathan Morton <jonathan.morton.ctr@usgs.gov>
2017-12-05 11:04:54 -0600

    Explain DB ns

--- src/lcmap/chipmunk/db.clj
+++ src/lcmap/chipmunk/db.clj
@@ -1,7 +1,6 @@
 (ns lcmap.chipmunk.db
   "Cassandra connections and helper functions."
-  (:require [clojure.java.io :as io]
-            [clojure.tools.logging :as log]
+  (:require [clojure.tools.logging :as log]
             [mount.core :refer [defstate] :as mount]
             [qbits.alia :as alia]
             [qbits.alia.codec.default :as default-codec]
@@ -9,16 +8,38 @@
   (:import org.joda.time.DateTime))
 
 
-(extend-protocol default-codec/Encoder org.joda.time.DateTime (encode [x] (.toDate x)))
+;; ## Overview
+;;
+;; This namespace defines the state and behavior of connections to
+;; the Cassandra cluster. It relies on values `lcmap.chipmunk.config`
+;; for specific values such as hostnames and credentials.
+;;
 
+;; ## Joda Support
+;;
+;; Enabling encoding of Joda types provides a degree of convenience.
+;;
+
+(extend-protocol default-codec/Encoder
+  org.joda.time.DateTime (encode [x] (.toDate x)))
+
+
+;; ## Declarations
+;;
+;;
 
 (declare db-cluster db-session)
 
 
-(defn db-cluster-start
-  "Open cluster connection.
+;; ## db-cluster
+;;
+;; After start `db-cluster` refers to com.datastax.driver.core.Cluster,
+;; an object that maintains general information about the cluster; use
+;; db-session to execute queries.
+;;
 
-  See also `db-session`."
+(defn db-cluster-start
+  "Open cluster connection."
   []
   (let [db-cfg (config/alia-config)]
     (log/debugf "start db cluster connection")
@@ -31,14 +52,20 @@
   (log/debugf "stop db cluster connection")
   (alia/shutdown db-cluster))
 
-;; After start this refers to com.datastax.driver.core.Cluster, an
-;; object that maintains general information about the cluster; use
-;; db-session to execute queries.
 
 (defstate db-cluster
   :start (db-cluster-start)
   :stop  (db-cluster-stop))
 
+
+;; ## db-session
+;;
+;; After start `db-session` refers to a com.datastax.driver.core.SessionManager
+;; object that can be used to execute queries.
+;;
+;; _WARNING: Do not use the same session for multiple keyspaces, functions
+;; that rely on this state expect a stable keyspace name!_
+;;
 
 (defn db-session-start
   "Create session that uses the default keyspace."
@@ -54,12 +81,6 @@
   (alia/shutdown db-session))
 
 
-;; After start this will refer to a com.datastax.driver.core.SessionManager
-;; object that can be used to execute queries.
-;;
-;; WARNING: Do not use the same session for multiple keyspaces, functions
-;; that rely on this state expect a stable keyspace name!
-;;
 (defstate db-session
   :start (db-session-start)
   :stop  (db-session-stop))
