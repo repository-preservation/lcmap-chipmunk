1c6679cc75d17646e4dfd5bd82811a5248ee25d4
Jonathan Morton <jonathan.morton.ctr@usgs.gov>
2017-12-09 14:23:44 -0600

    Update ports to 5858.
    * Update load script ports.
    * Update sample profile ports

--- profiles.example.clj
+++ profiles.example.clj
@@ -1,6 +1,6 @@
 {:dev  {:env {:db-host "localhost"
               :db-keyspace "chipmunk_local"
-              :http-port "5656"}}
+              :http-port "5858"}}
  :test {:env {:db-host "localhost"
               :db-keyspace "chipmunk_test"
               :http-port "5757"}}}
