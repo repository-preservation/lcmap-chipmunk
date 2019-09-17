ca2f97cf174cae7631fed5708d5efe22c2761dcd
David Hill <dhill@contractor.usgs.gov>
2019-09-17 10:55:10 -0500

    increment version to 1.0.3-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject chipmunk "1.0.2"
+(defproject chipmunk "1.0.3-SNAPSHOT"
   :description "Geospatial data ingest tools"
   :url "http://github.com/usgs-eros/lcmap-chipmunk"
   :license {:name "Unlicense"
